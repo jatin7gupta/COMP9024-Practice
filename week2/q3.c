0a73eef68e796b616fe6cd58fc62673f2935083b
z5240221 <z5240221@cse.unsw.edu.au>
2019-06-16 14:07:59 +1000

    w2 q3

--- week2/q3.c
+++ week2/q3.c
@@ -7,7 +7,17 @@ Written:  19.06.16
 #include <stdio.h>
 #include <stdlib.h>
 
- int main(void) { 
- 	 printf("hello world"); 
- 	 return EXIT_SUCCESS; 
+ int main(int argc, char *argv[]) { 
+ 	if (argc == 2) {
+ 		int i;
+ 		for (i = 0; i < atoi(argv[1]); i++) {
+ 			printf("%d,", i);
+ 		}
+ 		printf("%d\n", i);
+ 	} else {
+ 		fprintf(stderr, "Usage: ./q3.out number\n");
+ 		return EXIT_FAILURE;
+ 	}
+ 	 
+ 	return EXIT_SUCCESS; 
  }
