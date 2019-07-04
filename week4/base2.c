7d74ac0fe7cce80369b53006be21327537722b86
Jatin Gupta <guptajatin@outlook.com>
2019-07-04 14:06:01 +1000

    updated base

--- week4/base2.c
+++ week4/base2.c
@@ -11,19 +11,24 @@ Written(YY.MM.DD):  19.07.04
 int main(int argc, char *argv[]) {
 	int num;
 	Quack qs;
-	if (argc == 2 && sscanf(argv[1], "%d", &num) == 1) {
-		qs = createQuack();
-		while (num > 0) {
-			push(num % 2, qs);
-			num = num/2;
-		}
-		while (!isEmptyQuack(qs)) {
-			printf("%d", pop(qs));		
-		}
-		printf("\n");
+	if (argc == 2 && sscanf(argv[1], "%d", &num) == 1 && num >= 0) {
+		if (num != 0) {
+			qs = createQuack();
+			while (num > 0) {
+				push(num % 2, qs);
+				num = num/2;
+			}
+			while (!isEmptyQuack(qs)) {
+				printf("%d", pop(qs));		
+			}
+			printf("\n");
+		} 
+		else {
+			printf("0\n");
+		} 
 	} 
 	else {
-		fprintf(stderr, "Usage: number");
+		fprintf(stderr, "Usage: ./a.out number\n");
 		return EXIT_FAILURE;
 	}
 	
